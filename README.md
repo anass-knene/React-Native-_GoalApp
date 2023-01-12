0c8832a02abf3b48a28cbee5900dd65c99441dd5
‪Anass Knene‬‏ <81758940+anass-knene@users.noreply.github.com>
2023-01-12 16:17:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Landing page
+
+![alt text](./assets/image/landing_addGoal%20page.jpg)
+
+## Add GoalInput page
+
+![alt text](./assets/image/addGoal%20input%20padge.jpg)
+
+## Adding Goal
+
+![alt text](./assets/image/adding%20Goal.jpg)
+
+## Add Goal to landing page
+
+![alt text](./assets/image/Goal%20add%20in%20landing%20page.jpg)
---
+++ components/GoalItem.js
@@ -0,0 +1,34 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+StyleSheet;
+
+function GoalItem(props) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#5e0acc" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  goalText: {
+    padding: 8,
+    color: "white",
+  },
+});
